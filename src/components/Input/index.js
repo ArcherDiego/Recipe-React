28adfbba3bb84e5262ad1e03a0f263e96953417b
unknown <doliveirasantos98@gmail.com>
2021-10-29 20:15:27 -0300

    refactor(input): state input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,11 +1,18 @@
 import StyleInput from "./style"
 import Button from "./../Button"
+import { useState } from "react"
 
 const Input = () => {
+
+    const [ search, setSearch ] = useState('')
+    const handleChange = ({target}) => {
+        setSearch(target.value)
+    }
+
     return (
         <StyleInput>
             <label htmlFor='search'>Search Recipe: </label>
-            <input id='search' type='text'></input>
+            <input name="search" value={ search } type="text" onChange={ handleChange } />
             <Button name={ '>' } />
         </StyleInput>
     )
